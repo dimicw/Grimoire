75d7576a04f338f8c831748963d68d786d66c1f4
dimicw <104204285+dimicw@users.noreply.github.com>
2023-08-28 20:09:45 +0200

    spell card base

--- app/src/main/java/com/example/grimuare/SpellCardActivity.java
+++ app/src/main/java/com/example/grimuare/SpellCardActivity.java
@@ -3,17 +3,36 @@ package com.example.grimuare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SpellCardActivity extends AppCompatActivity {
+
+    ImageButton backButton;
+    ImageView backgroundImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spell_card);
 
+        backButton = findViewById(R.id.backButton);
+        backgroundImage = findViewById(R.id.backgroundImage);
+
+        backgroundImage.setImageResource(
+             getIntent().getIntExtra("IMAGE", 0));
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
         String name = getIntent().getStringExtra("NAME");
-        TextView tv = findViewById(R.id.textView);
+        TextView tv = findViewById(R.id.nameCard);
         tv.setText(name);
     }
 }
